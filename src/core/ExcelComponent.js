62677de352919f9e0efea5e5b5b2d5389f391150
Vladislav Holovko <gsfrontmail@gmail.com>
2022-07-02 22:17:02 +0300

    finish table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,7 +4,12 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.emitter = options.emitter;
+    this.unsubscribers = [];
+    this.prepare();
   }
+
+  prepare() {}
 
   /**
    * Return HTML template
@@ -14,11 +19,21 @@ export class ExcelComponent extends DomListener {
     return '';
   }
 
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+
+  $on(event, callback) {
+    const unsubscribe = this.emitter.subscribe(event, callback);
+    this.unsubscribers.push(unsubscribe);
+  }
+
   init() {
     this.initDOMListeners();
   }
 
   destroy() {
     this.removeDOMListeners();
+    this.unsubscribers.forEach(unsub => unsub());
   }
 }
